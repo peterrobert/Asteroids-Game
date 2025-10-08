af782098146e17263a4ed43e54068f401e4b54f3
peterrobert <pwambui93@gmail.com>
2025-10-08 11:10:53 +0300

    Created the constant file that stores magic numbers for the game

---
+++ constants.py
@@ -0,0 +1,8 @@
+# -- In this file, you can define constants for your game, such as screen dimensions, colors, and game settings. (Magic Numbers) --
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_KINDS = 3
+ASTEROID_SPAWN_RATE = 0.8  # seconds
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS

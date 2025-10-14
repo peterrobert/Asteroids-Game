0125d95099e359db4071c86d123d5ab18f1f70a7
peterrobert <pwambui93@gmail.com>
2025-10-14 06:58:50 +0300

    Created the triangle to represent the player. Added a player class

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    # --- player instance ---
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return # Exit the program
-                
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        time = clock.tick(60)
+        dt = time / 1000  # Convert milliseconds to seconds
+       
 
 
 
